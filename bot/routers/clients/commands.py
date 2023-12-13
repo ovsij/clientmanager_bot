0c0c2e5504f09bc4debe08bc5d93c16cc9e9a36f
ovsij <v.ovsij@yandex.ru>
2023-12-13 20:28:15 +0300

    first commit

--- bot/routers/clients/commands.py
+++ bot/routers/clients/commands.py
@@ -9,6 +9,6 @@ clients_commands_router = Router()
 @clients_commands_router.message(Command(commands=["start"]))
 async def add_command(message: Message):
     
-    text_and_data = [['Открыть веб-приложение', 'https://rawcdn.githack.com/ovsij/clientmanager_bot/a5d3b9dd79a6cae63b4cd908ee10910a6b04fe30/bot/invoice_form.html']]
+    text_and_data = [['Открыть веб-приложение', 'https://ovsij.github.io/clientmanager_bot/bot/invoice_form.html']]
     reply_markup = InlineConstructor.create_kb(text_and_data, button_type=['web_app'])
     await message.answer("Чтобы добавить пользователя, нажмите кнопку ниже", reply_markup=reply_markup)
